28365f6ade95a5b44123f0883d55f723bd0c38dc
damon <915897364@qq.com>
2024-04-07 21:37:43 +0800

    gemini

--- api/main.py
+++ api/main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import google.generativeai as genai
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
+@app.route('/gemini/chat', methods=['POST'])
+def gemini_chat(data: dict):
+    prompt = data.get('prompt')
+    api_key= data.get('api_key')
+    try:
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel('gemini-pro')
+        chat = model.start_chat(history=[])
+        response = chat.send_message(prompt)
+        text=response.text
+        response = {"content": text}
+        return response
+    
+    except Exception as e:
+        print(e)
+        return None
 
 if __name__ == '__main__':
     app.run()
