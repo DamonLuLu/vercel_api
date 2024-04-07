6b20b224b6bc97e0f43f99148ac88b28a0b137f9
damon <915897364@qq.com>
2024-04-07 21:09:34 +0800

    BUg

--- api/main.py
+++ api/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI
-# from mangum import Mangum
-import uvicorn
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-# handler = Mangum(app)
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1",workers=1)
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
